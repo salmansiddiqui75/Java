8d15d6b1ede1c83457b14456e446e04e9d7fa06a
Salman Siddiqui <81064986+salmansiddiqui75@users.noreply.github.com>
2022-02-01 22:35:33 +0530

    Add files via upload

---
+++ Array/Array1/src/AA.java
@@ -0,0 +1,15 @@
+
+public class AA {
+	public static void main(String[] args) {
+		
+	
+
+	String []name= {"Amit","Suresh","Hemant"};
+	String []str= {"First","Second","Third "};
+	for(int i=0;i<name.length;i++)
+	{
+		System.out.println("Name of "+str[i]+" person is "+name[i]);
+	}
+
+}
+}
